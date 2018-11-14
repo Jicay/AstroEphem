1c2a46d36a3b28b63b07acdca77cda6c1965d51f
Jicay <jeanclement.sabourin@gmail.com>
2018-11-14 13:56:43 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from app.web.planet import Planet
 from app.web.day import Day
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(app)
 
 api.add_resource(Planet, '/planets', '/planets/<name>', methods=['GET'])
